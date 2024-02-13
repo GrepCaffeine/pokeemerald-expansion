8aa98ec249eedc7e83203da54ce7bf15c008ac6d
GrepCaffeine <grepcaffeine@pm.me>
2024-02-13 13:38:12 -0600

    Config Edits

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -2,7 +2,7 @@
 #define GUARD_CONFIG_OVERWORLD_H
 
 // Movement config
-#define OW_RUNNING_INDOORS          GEN_LATEST  // In Gen4+, players are allowed to run indoors.
+#define OW_RUNNING_INDOORS          FALSE  // In Gen4+, players are allowed to run indoors.
 
 // Other settings
 #define OW_POISON_DAMAGE            GEN_LATEST // In Gen4, Pokémon no longer faint from Poison in the overworld. In Gen5+, they no longer take damage at all.
